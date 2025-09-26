c1f5013e78bc72e3d98193a09d7514ae660a363b
nt <>
2025-09-26 22:35:49 +0300

    loggingu

--- termux/sync.py
+++ termux/sync.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import subprocess
+import logging as log
 import os
 
 SYNC_DIRS = {
@@ -13,6 +14,11 @@ SYNC_DIRS = {
 
 DEST = "foo@192.168.0.1:/ssd/phone"
 
+log.basicConfig(
+	level = log.DEBUG,
+	format = '\033[36m%(asctime)s \033[33m%(levelname)5s \033[0m%(message)s'
+)
+
 def rsync_sync(source, dest, excludes=None, remove_source=True):
     cmd = ["rsync", "-avzt"]
     if remove_source:
@@ -22,10 +28,11 @@ def rsync_sync(source, dest, excludes=None, remove_source=True):
             cmd.extend(["--exclude", ex])
     cmd.extend([source, dest])
 
-    print("exec:", " ".join(cmd))
+    log.debug(f"{source} {excludes}")
+    log.debug(f"exec: {" ".join(cmd)}")
     result = subprocess.run(cmd)
     if result.returncode != 0:
-        print(f"sync error {source}")
+        log.error(f"sync error {source}")
 
 def remove_empty_dirs(path):
     for root, dirs, files in os.walk(path, topdown=False):
@@ -33,16 +40,16 @@ def remove_empty_dirs(path):
             dir_path = os.path.join(root, d)
             if not os.listdir(dir_path):
                 os.rmdir(dir_path)
-                print(f"rm -rf empty dir: {dir_path}")
+                log.info(f"rm -rf empty dir: {dir_path}")
 
 def main():
-    print("start sync...")
+    log.info("start sync...")
 
     for dir_path, excludes in SYNC_DIRS.items():
         rsync_sync(dir_path, DEST, excludes=excludes, remove_source=True)
         remove_empty_dirs(dir_path)
 
-    print("sync completed!")
+    log.info("sync completed!")
 
 if __name__ == "__main__":
     main()
